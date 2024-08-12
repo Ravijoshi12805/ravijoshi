1bba92a5caeed3fc6a98e6cc5b7e4b53d9805894
Ravi Joshi <joshiravi1282005@gmail.com>
2024-08-12 09:58:41 +0530

    hii i am ravi joshi

---
+++ Day 2/readme.md
@@ -0,0 +1,2 @@
+<!-- Note inline, blick and void element>
+<!-- Note inline = <a>,<img>>
---
+++ Day 7/index.js
@@ -0,0 +1,10 @@
+console.log('git verson 1');
+console.log('git version 1 new line');
+
+
+
+
+//git init
+//git status
+//git add. //staging area me add karne k liye
+//git commit -m "msg"
